5ee1ce840521b63ebb51f64149b3c8a278441943
Abelj <111173212+abeljemal22@users.noreply.github.com>
2022-11-29 22:58:59 +0300

    commit1

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for alpha_letters in range(ord('a'), ord('z')):
-    print("{}".format(chr(alpha_letters)+1), end="")
+for i in range(97, 123):
+    print("{}".format(chr(i)), end='')
