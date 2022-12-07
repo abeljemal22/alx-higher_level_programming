ccc970da7b27b13c1d4799a1790bb1bf9c80904b
abeljemal22 <abel.j3938@gmail.com>
2022-12-06 20:38:49 -0800

    pmdata

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+ght_average(my_list=[]):
+    """ returns the weighted average"""
+    """ of all integers tuple (<score>, <weight>)"""
+    if not my_list:
+        return 0
+    num = 0
+    den = 0
+    for tup in my_list:
+        num += tup[0] * tup[1]
+        den += tup[1]
+        return (num / den)
