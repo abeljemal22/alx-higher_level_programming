8064bf30ce80ccb3daf44b805c65b71de2f256fa
abeljemal22 <abel.j3938@gmail.com>
2023-01-02 03:30:56 -0800

    pmclass

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Defines a Rectangle class."""
+
+
+
+
+
+class Rectangle:
+
+        """Represent a rectangle."""
+
+
+
+            def __init__(self, width=0, height=0):
+
+                        """Initialize a new Rectangle.
