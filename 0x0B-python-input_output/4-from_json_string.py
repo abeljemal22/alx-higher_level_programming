493636e58e0780077377dee89bbf2932c331df08
Abelj <111173212+abeljemal22@users.noreply.github.com>
2023-01-10 09:31:18 +0300

    pyio

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,9 @@
-4-from_json_string.py
+#!/usr/bin/python3
+
+"""Defines a JSON-to-object function."""
+import json
+
+
+def from_json_string(my_str):
+    """Return the Python object representation of a JSON string."""
+    return json.loads(my_str)
